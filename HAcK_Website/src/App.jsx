ae0b4dfec7c727c9cdc3f773e51d04944b89690e
tacgia2711 <tacgia2711@gmail.com>
2026-08-07 15:21:23 -0700

    Added a place to insert picture and the name of the instrument in javascript. mMade additional change to the CSS of the picture and name placeholder. Please put picture and name once the final product is finished.

--- HAcK_Website/src/App.jsx
+++ HAcK_Website/src/App.jsx
@@ -1,61 +1,84 @@
 import './App.css'
 
 function App() {
+    return (
+        <div className = "HAcK_Website">
+            <div className = "HomeSection">
 
-  return (
-      <div className = "HAcK_Website">
-          <div className = "HomeSection">
-              <h1 className="HackTitle">HAcK 2026</h1>
-              <h1 className="GroupName">The Stradibearius Quartet</h1>
-              
-          </div>
+                <h1 className="HackTitle">HAcK 2026</h1>
 
-          <div className = "MusicSection">
-          </div>
+                <h1 className="GroupName">The Stradibearius Quartet</h1>
+                
+            </div>
 
-          <div className = "BandSection">
-              <div className = "MemberCard">
-                  <h3>Kien Nguyen</h3>
-                  <p>Role: Web Developer</p>
-              </div>
-              <div className = "MemberCard">
-                  <h3>Matthew Kim</h3>
-                  <p>Role: Circuit Designer</p>
-              </div>
-              <div className = "MemberCard">
-                  <h3>Chris Penick</h3>
-                  <p>Role: Instruments chassis Assembler/Designer</p>
-              </div>
-              <div className = "MemberCard">
-                  <h3>Kevin Alvarado</h3>
-                  <p>Role: Circuit Designer - Tester</p>
-              </div>
-          </div>
+            <div className = "MusicSection">
 
-          <div className = "InstrumentSection">
-              <h2 className='InstrumentTitle'>Instruments Info</h2>
-              <div className = "InstrumentInfo">
-                  <div className = "InstrumentSubSec">
-                      <h3>Sound Profile</h3>
-                      <p>N/A - Waiting for instruments</p>
-                  </div>
-                  <div className = "InstrumentSubSec">
-                      <h3>Effects</h3>
-                      <p>N/A - Waiting for instruments</p>
-                  </div>
-                  <div className = "InstrumentSubSec">
-                      <h3>Sound Section</h3>
-                      <p>N/A - Waiting for instruments</p>
-                  </div>
-              </div>
-          </div>
+            </div>
 
-          <div className = "LiveSection">
+            <div className = "BandSection">
 
-          </div>
+                <div className = "MemberCard">
+                    <h3>Kien Nguyen</h3>
+                    <p>Role: Web Developer</p>
+                </div>
 
-      </div>
-  )
+                <div className = "MemberCard">
+                    <h3>Matthew Kim</h3>
+                    <p>Role: Circuit Designer</p>
+                </div>
+
+                <div className = "MemberCard">
+                    <h3>Chris Penick</h3>
+                    <p>Role: Instruments chassis Assembler/Designer</p>
+                </div>
+
+                <div className = "MemberCard">
+                    <h3>Kevin Alvarado</h3>
+                    <p>Role: Circuit Designer - Tester</p>
+                </div>
+            </div>
+
+            <div className = "InstrumentSection">
+
+                <h2 className='InstrumentTitle'>Instruments Info</h2>
+                
+                <div className="InstrumentImage">
+                    
+                    <p>
+                        Place picture of instrument here
+                    </p>
+
+                </div>
+
+                <h2 className="InstrumentName">Name of the instrument here</h2>
+                
+                <div className = "InstrumentInfo">
+
+                    <div className = "InstrumentSubSec">
+                        <h3>Sound Profile</h3>
+                        <p>N/A - Waiting for instruments</p>
+                    </div>
+
+                    <div className = "InstrumentSubSec">
+                        <h3>Effects</h3>
+                        <p>N/A - Waiting for instruments</p>
+                    </div>
+
+                    <div className = "InstrumentSubSec">
+                        <h3>Sound Section</h3>
+                        <p>N/A - Waiting for instruments</p>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div className = "LiveSection">
+
+            </div>
+
+        </div>
+    )
 }
 
 export default App
