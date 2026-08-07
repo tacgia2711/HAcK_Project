d3d62c3736fad51df769bb7ffab74554017786ab
tacgia2711 <tacgia2711@gmail.com>
2026-08-07 16:35:34 -0700

    Added a rotating 3D into the Live Mode to test out the three.js function.

--- HAcK_Website/src/Live.jsx
+++ HAcK_Website/src/Live.jsx
@@ -1,4 +1,6 @@
 import './App.css'
+import { Canvas,useFrame } from '@react-three/fiber'
+import { useRef,useState } from 'react'
 
 function Live({onExit}) {
     return(
@@ -7,11 +9,33 @@ function Live({onExit}) {
             <h1>Live Mode</h1>
 
             <div className="LiveInterface">
-                <p>Visualizer here</p>
+                <Canvas camera={{ fov:75, position:[0, 0, 2] }}>
+                    <ambientLight intensity={1}/>
+                    <directionalLight
+                        position = {[2,2,2]} intensity = {2}
+                    />
+                    <Cube />
+                </Canvas>
             </div>
 
             <button onClick={onExit}>Exit</button>
         </div>
+    )
+}
+
+function Cube() {
+    const cubeRef = useRef()
+
+    useFrame(() => {
+        cubeRef.current.rotation.x += 0.01
+        cubeRef.current.rotation.y += 0.01
+    })
+
+    return (
+        <mesh ref={cubeRef}>
+            <boxGeometry args={[1.5,1.5,1.5]}/>
+            <meshStandardMaterial color="yellow" />
+        </mesh>
     )
 }
 
