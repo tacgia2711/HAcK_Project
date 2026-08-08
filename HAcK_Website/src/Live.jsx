bb4c35373072a1f4573fe351e1944a7e8af87056
tacgia2711 <tacgia2711@gmail.com>
2026-08-07 17:57:02 -0700

    Added additional functional buttons to the cube so that it not just changing colors, including rotation speed and size. Will replacing these features with the matching function of the instrument in the future.

--- HAcK_Website/src/Live.jsx
+++ HAcK_Website/src/Live.jsx
@@ -10,7 +10,7 @@ function Live({onExit}) {
             <h1>Live Mode</h1>
 
             <div className="LiveInterface">
-                <Canvas camera={{ fov:75, position:[0, 0, 2] }}>
+                <Canvas camera={{ fov:85, position:[0, 0, 2] }}>
                     <ambientLight intensity={1}/>
                     <directionalLight
                         position = {[2,2,2]} intensity = {2}
@@ -28,6 +28,8 @@ function Live({onExit}) {
                 <button onClick={() => setNote('A')}>A</button>
                 <button onClick={() => setNote('B')}>B</button>
             </div>
+
+            <h2>Current Note: {currentNote}</h2>
             
             <button onClick={onExit}>Exit</button>
         </div>
@@ -38,13 +40,31 @@ function Live({onExit}) {
 function Cube({Note}) {
     const cubeRef = useRef()
 
+    const scale =   Note === 'C' ? 1.1: 
+                    Note === 'D' ? 1.2:
+                    Note === 'E' ? 1.3:
+                    Note === 'F' ? 1.4: 
+                    Note === 'G' ? 1.5:
+                    Note === 'A' ? 1.6:
+                    Note === 'B' ? 1.7:
+                    1
+
+    const rotationSpeed =   Note === 'C' ? 0.005: 
+                            Note === 'D' ? 0.010:
+                            Note === 'E' ? 0.015:
+                            Note === 'F' ? 0.020: 
+                            Note === 'G' ? 0.025:
+                            Note === 'A' ? 0.030:
+                            Note === 'B' ? 0.035:
+                            0.001
     useFrame(() => {
-        cubeRef.current.rotation.x += 0.01
-        cubeRef.current.rotation.y += 0.01
+        cubeRef.current.rotation.x += rotationSpeed
+        cubeRef.current.rotation.y += rotationSpeed
     })
 
     return (
-        <mesh ref={cubeRef}>
+        <mesh ref={cubeRef}
+            scale={[scale, scale, scale]}>
             <boxGeometry args={[1.5,1.5,1.5]}/>
             <meshStandardMaterial color={Note === 'C' ? 'yellow': 
                                          Note === 'D' ? 'orange':
