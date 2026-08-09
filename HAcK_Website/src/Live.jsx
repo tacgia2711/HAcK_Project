41696e0bce897ad3c47d4e73c09aba6a010c6ddb
tacgia2711 <tacgia2711@gmail.com>
2026-08-08 20:40:17 -0700

    Removed the cube in the LiveMode and all of its related function. Added the waves feature. Reused any usable code from previous live cube.

--- HAcK_Website/src/Live.jsx
+++ HAcK_Website/src/Live.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import { Canvas, useFrame } from '@react-three/fiber'
 import { useRef, useState, useEffect } from 'react'
 
 function Live({onExit}) {
@@ -44,15 +43,12 @@ function Live({onExit}) {
 
             <h1>Live Mode</h1>
 
-            <div className="LiveInterface">
-                <Canvas camera={{ fov:85, position:[0, 0, 2] }}>
-                    <ambientLight intensity={1}/>
-                    <directionalLight
-                        position = {[2,2,2]} intensity = {2}
-                    />
-                    <Cube Note={currentNote} Effects = {currentEffect}/>
-                </Canvas>
-            </div>
+           <div className = "LiveWaves">
+                <Wave
+                    Note = {currentNote}
+                    Effects = {currentEffect}
+                />
+           </div>
 
             <div>
                 <button onClick={() => setNote('C4')}>C</button>
@@ -61,7 +57,7 @@ function Live({onExit}) {
                 <button onClick={() => setNote('D#4')}>F</button>
                 <button onClick={() => setNote('E4')}>G</button>
                 <button onClick={() => setNote('F4')}>A</button>
-                <button onClick={() => setNote('B')}>B</button>
+                <button onClick={() => setNote('B4')}>B</button>
             </div>
 
             <div className="EffectControl">
@@ -86,61 +82,47 @@ function Live({onExit}) {
     )
 }
 
-function Cube({Note, Effects}) {
-    const cubeRef = useRef()
+function Wave({Note, Effects}) {
+    const mainWave = useRef()
+    const echoWave1 = useRef()
+    const echoWave2 = useRef()
 
-    const scale =   Note === 'C4' ? 0.9: 
-                    Note === 'C#4' ? 1.0:
-                    Note === 'D4' ? 1.1:
-                    Note === 'D#4' ? 1.2: 
-                    Note === 'E4' ? 1.3:
-                    Note === 'F4' ? 1.4:
-                    Note === 'F#4' ? 1.5:
-                    Note === 'G4' ? 1.6:
-                    Note === 'G#4' ? 1.7:
-                    Note === 'A4' ? 1.8:
-                    Note === 'A#4' ? 1.9:
-                    Note === 'B4' ? 2.0:
-                    0.1
-
-    const rotationSpeed =   Note === 'C4' ? 0.0005: 
-                            Note === 'C#4' ? 0.0010:
-                            Note === 'D4' ? 0.0015:
-                            Note === 'D#4' ? 0.0025: 
-                            Note === 'E4' ? 0.0030:
-                            Note === 'F4' ? 0.0035:
-                            Note === 'F#4' ? 0.0040:
-                            Note === 'G4' ? 0.0045:
-                            Note === 'G#4' ? 0.0050:
-                            Note === 'A4' ? 0.0060:
-                            Note === 'A#4' ? 0.0065:
-                            Note === 'B4' ? 0.0070:
-                            1
+    const index =   Note === 'C4' ? 0: 
+                    Note === 'C#4' ? 1:
+                    Note === 'D4' ? 2:
+                    Note === 'D#4' ? 3: 
+                    Note === 'E4' ? 4:
+                    Note === 'F4' ? 5:
+                    Note === 'F#4' ? 5:
+                    Note === 'G4' ? 7:
+                    Note === 'G#4' ? 8:
+                    Note === 'A4' ? 9:
+                    Note === 'A#4' ? 10:
+                    Note === 'B4' ? 11:
+                    12
+    
+    const waveColor =   Note === 'C4'  ? 'yellow' :
+                        Note === 'C#4' ? 'gold' :
+                        Note === 'D4'  ? 'orange' :
+                        Note === 'D#4' ? 'coral' :
+                        Note === 'E4'  ? 'red' :
+                        Note === 'F4'  ? 'lime' :
+                        Note === 'F#4' ? 'green' :
+                        Note === 'G4'  ? 'cyan' :
+                        Note === 'G#4' ? 'blue' :
+                        Note === 'A4'  ? 'purple' :
+                        Note === 'A#4' ? 'magenta' :
+                        Note === 'B4'  ? 'pink' :
+                        'white'
 
     useFrame(() => {
-        cubeRef.current.rotation.x += rotationSpeed
-        cubeRef.current.rotation.y += rotationSpeed
+
     })
 
     return (
-        <mesh ref={cubeRef}
-            scale={[scale, scale, scale]}>
-            <boxGeometry args={[1.5,1.5,1.5]}/>
-            <meshStandardMaterial color={   Note === 'C4'  ? 'yellow' :
-                                            Note === 'C#4' ? 'gold' :
-                                            Note === 'D4'  ? 'orange' :
-                                            Note === 'D#4' ? 'coral' :
-                                            Note === 'E4'  ? 'red' :
-                                            Note === 'F4'  ? 'lime' :
-                                            Note === 'F#4' ? 'green' :
-                                            Note === 'G4'  ? 'cyan' :
-                                            Note === 'G#4' ? 'blue' :
-                                            Note === 'A4'  ? 'purple' :
-                                            Note === 'A#4' ? 'magenta' :
-                                            Note === 'B4'  ? 'pink' :
-                                            'white'
-            } />
-        </mesh>
+        <div>
+            
+        </div>
     )
 }
 
